92e35b1717a21b113c71ce065ad5d3cacb092d08
lovelysusie <haonanshihaoren@126.com>
2017-02-06 11:52:18 +0800

    made changes

--- scaleR.R
+++ scaleR.R
@@ -15,67 +15,64 @@ wholeData <- rxDataStep(inData = wholeData,
                         varsToDrop = varsToDrop,
                         overwrite = TRUE)
 
+wholeData$hubid =as.character(wholeData$hubid)
+
 hublist = table(wholeData$hubid)
+hublist = data.frame(hublist)
+hublist$Var1 <-as.character(hublist$Var1)
+hublist <-rxDataStep(inData = hublist, outFile = 'hublist.xdf')
+hublist <-rxDataStep(inData = hublist)
 
-
-wholeData <- rxDataStep(inData = wholeData, 
-                        varsToDrop = varsToDrop,
-                        rowSelection = (tasklocation == 'Bedroom' & 
-                                          hubid == 'SG-04-avent001'), 
+partOne <- rxDataStep(inData = wholeData, 
+                        rowSelection = (tasklocation == "Bedroom" &
+                                         hubid==unique(hubid)[2]), 
                         overwrite = TRUE)
-wholeData$starttime = substr(wholeData$starttime,1,19)
-wholeData$starttime = sub("T", " ",wholeData$starttime, fixed=FALSE)
-wholeData$starttime <-strptime(wholeData$starttime, "%Y-%m-%d %H:%M:%S")
-wholeData$starttime <-as.POSIXct(wholeData$starttime)
-wholeData$date = as.Date(wholeData$starttime)
 
-wholeData <- rxDataStep(inData = wholeData, 
-                        rowSelection = (date == Sys.Date() | date == Sys.Date()-1), 
-                        overwrite = TRUE)
+partOne$starttime = substr(partOne$starttime,1,19)
+partOne$starttime = sub("T", " ",partOne$starttime, fixed=FALSE)
+partOne$starttime <-strptime(partOne$starttime, "%Y-%m-%d %H:%M:%S")
+partOne$starttime <-as.POSIXct(partOne$starttime)
+partOne$date = as.Date(partOne$starttime)
+
+partOne <- rxDataStep(inData = partOne, 
+                      rowSelection = (date == Sys.Date()|date == Sys.Date()-1), 
+                      overwrite = TRUE)
 
 
 #get the starting of the 30min interval
-wholeData$status1 <-"duration"
+partOne$status1 <-"duration"
 i=1
 
-while (i<nrow(wholeData)) {
-  if (difftime(wholeData$starttime[i+1],wholeData$starttime[i], units = "min")>=30) wholeData$status1[i]="start" 
+while (i<nrow(partOne)) {
+  if (difftime(partOne$starttime[i+1],partOne$starttime[i], units = "min")>=30) partOne$status1[i]="start" 
   i=i+1
 }
 ###get the endin of the 30min interval
-wholeData$status2 <-"duration"
+partOne$status2 <-"duration"
 i=1
 
-while (i<nrow(wholeData)) {
-  if (difftime(wholeData$starttime[i+1],wholeData$starttime[i], units = "min")>=30) wholeData$status2[i+1]="end" 
+while (i<nrow(partOne)) {
+  if (difftime(partOne$starttime[i+1],partOne$starttime[i], units = "min")>=30) partOne$status2[i+1]="end" 
   i=i+1
 }
-##############################
-sleepAnalysis <-data.frame(date = Sys.Date(), wakeup="07:30:01", sleep="21:59:59")
-sleepAnalysis$wakeup <-paste(sleepAnalysis$date,sleepAnalysis$wakeup)  
-sleepAnalysis$wakeup <-strptime(sleepAnalysis$wakeup, "%Y-%m-%d %H:%M:%S")
-sleepAnalysis$wakeup <-as.POSIXct(sleepAnalysis$wakeup)
-
-sleepAnalysis$sleep <-paste(sleepAnalysis$date,sleepAnalysis$sleep)  
-sleepAnalysis$sleep <-strptime(sleepAnalysis$sleep, "%Y-%m-%d %H:%M:%S")
-sleepAnalysis$sleep <-as.POSIXct(sleepAnalysis$sleep)
-
-##############################
-starting <- rxDataStep(inData = wholeData, 
+########################
+starting <- rxDataStep(inData = partOne, 
                        rowSelection = (status1 == 'start'), 
                        overwrite = TRUE)
 
-ending <- rxDataStep(inData = wholeData, 
+ending <- rxDataStep(inData = partOne, 
                      rowSelection = (status2 == 'end'), 
                      overwrite = TRUE)
+
+sleepAnalysis <-rxDataStep(inData = sleepAnalysis,overwrite = TRUE)
 
 flag=sleepAnalysis$wakeup
 
 starting <- rxDataStep(inData = starting, 
-                       rowSelection = (starttime < flag), 
+                       rowSelection = (starttime < strptime(paste(Sys.Date(),"07:30:01"), "%Y-%m-%d %H:%M:%S")), 
                        overwrite = TRUE)
 ending <- rxDataStep(inData = ending, 
-                     rowSelection = (starttime < sleepAnalysis$wakeup[1]), 
+                     rowSelection = (starttime < strptime(paste(Sys.Date(),"07:30:01"), "%Y-%m-%d %H:%M:%S")), 
                      overwrite = TRUE)
 
 sleepAnalysis$wakeup[1] <-tail(ending$time,1)
