1273679b2443261e9431c1650f9e0bb6469f609f
lovelysusie <haonanshihaoren@126.com>
2017-02-08 12:30:32 +0800

    made changes

--- stream_analytics.sql
+++ stream_analytics.sql
@@ -1,25 +1,27 @@
-WITH HDB04 AS (
+WITH adventis01 AS (
   SELECT
-        *,
-       
+       IoTHub.ConnectionDeviceId AS hubid,
+       EventProcessedUtcTime AS eventtime,
+       taskLocation,
        LAG([EventProcessedUtcTime]) OVER (LIMIT DURATION(hour, 3)) as previousTime,
        LAG([taskLocation]) OVER (LIMIT DURATION(hour, 3)) as formertaskLocation
   FROM
       hdbinput TIMESTAMP BY EventProcessedUtcTime
       WHERE name = 'movement' AND taskName = 'movement' 
-   
-      )
-  SELECT 
-  
-      System.TimeStamp AS Endtime,
-       
+)
+
+SELECT 
+      tasklocation,
+      hubid, 
+      System.TimeStamp AS Endtime, 
       SUM(DATEDIFF(second, previousTime, EventProcessedUtcTime)) AS timegap
-      FROM HDB04 
+      FROM adventis01 
       WHERE
-        [taskLocation] = 'Bathroom' AND formertaskLocation = 'Bathroom' AND address = '01:84:D6:E0:00:05'
+        [taskLocation] = 'Bathroom' AND formertaskLocation = 'Bathroom' 
         AND DATEDIFF(second, previousTime, EventProcessedUtcTime) > 10
     GROUP BY TumblingWindow(minute, 1)
     
 
-SELECT testlocation(h.tasklocation)
-FROM hdbinput h
+GROUP BY
+        IoTHub.ConnectionDeviceId
+      
