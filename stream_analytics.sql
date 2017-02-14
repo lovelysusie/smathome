88ee5baa9bdb51b0f8f6795ceaaf0e1bd1bb166e
lovelysusie <haonanshihaoren@126.com>
2017-02-14 10:09:53 +0800

    made changes

--- stream_analytics.sql
+++ stream_analytics.sql
@@ -10,24 +10,24 @@ WITH fivemintable AS(
     FROM
         gateway TIMESTAMP BY EventProcessedUtcTime
     WHERE
-        (name = 'movement' AND taskName = 'movement' AND IoTHub.ConnectionDeviceId = 'SG-04-HDB00013') 
-        OR (name = 'heaartbeat' AND IoTHub.ConnectionDeviceId = 'SG-04-HDB00013')
+        (name = 'movement' AND taskName = 'movement' ) 
+        OR (name = 'heaartbeat')
 )
 
 SELECT 
     name,
     hubid,
     eventtime,
-    LAG([eventtime]) OVER (LIMIT DURATION(hour, 3)) as previousTime,
-    LAG([taskLocation],4) OVER (LIMIT DURATION(hour,3 )) as formertaskLocation,
-    LAG([name],3) OVER (LIMIT DURATION(hour, 3)) as preprepreName,
-    LAG([name],2) OVER (LIMIT DURATION(hour, 3)) as prepreName,
-    LAG([name],1) OVER (LIMIT DURATION(hour, 3)) as preName
+    LAG([eventtime]) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) as previousTime,
+    LAG([taskLocation],4) OVER (PARTITION BY hubid LIMIT DURATION(hour,3 )) as formertaskLocation,
+    LAG([name],3) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) as preprepreName,
+    LAG([name],2) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) as prepreName,
+    LAG([name],1) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) as preName
       
 FROM fivemintable 
-WHERE name = 'heartbeat' 
-      AND LAG([taskLocation],4) OVER (LIMIT DURATION(hour, 3)) = 'Bedroom'
-      AND LAG(name,3) OVER (LIMIT DURATION(hour, 3)) = 'heartbeat'
-      AND LAG(name,2) OVER (LIMIT DURATION(hour, 3)) = 'heartbeat'
-      AND LAG(name,1) OVER (LIMIT DURATION(hour, 3)) = 'heartbeat'
+WHERE name = 'movement' 
+      AND LAG([taskLocation],4) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) = 'Bedroom'
+      AND LAG(name,3) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) = 'heartbeat'
+      AND LAG(name,2) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) = 'heartbeat'
+      AND LAG(name,1) OVER (PARTITION BY hubid LIMIT DURATION(hour, 3)) = 'heartbeat'
        
