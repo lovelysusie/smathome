3e8ddfbbb4663a1268faa378a94bb49d2e393c1e
lovelysusie <haonanshihaoren@126.com>
2017-03-03 11:58:23 +0800

    made changes

--- pyTest.py
+++ pyTest.py
@@ -4,26 +4,18 @@
 account_name='blobsensordata'
 account_key='zUYv9mIC9KPr/k+Sa15y4mN6mtozuJcF/n979cqojT4HaMUj3ahEHaPBVtpDihwfO78JTk8sQ29xCaxGWfjtSA=='
 #container_name = 'preprocessed-data'
-import time
 from azure.storage.blob import BlockBlobService
-from azure.storage.blob import PublicAccess
+#from azure.storage.blob import PublicAccess
+import pandas as pd
+from io import StringIO
 #from azure.storage.blob import PublicAccess
 blob_service = BlockBlobService(account_name=account_name, account_key = account_key)
 #blob_service.get_blob_to_path("rspark","blobname","localfilename")
-blob_service.create_container('mycontainer', public_access=PublicAccess.Container)
-todayDate = time.strftime("%Y-%m-%d")
-print("hello world")
-from datetime import datetime, timedelta
-
-yesterday = datetime.now() - timedelta(days=1)
-yesterday = yesterday.strftime("%Y-%m-%d")
-
+#blob_service.create_container('mycontainer', public_access=PublicAccess.Container)
 container_name = 'adventis-input'
-#container_name = 'preprocessed-data'
+container_name = 'mycontainer'
 #blob_name ='2017-03-02/326454415_86a7973c64a7481784acfcd9578bd964_1.csv'
 #blob_string = blob_service.get_blob_to_text(container_name=container_name, blob_name=blob_name)
-
-containers = blob_service.list_blob(container_name=container_name)
 
 blobs = []
 marker = None
@@ -35,5 +27,31 @@ while True:
     marker = batch.next_marker
 for blob in blobs:
     print(blob.name)
-blob_string = blob_service.get_blob_to_text(container_name=container_name, blob_name = blobs[0].name)
-    
+TodayNo = len(blobs)-1    
+YstNo = len(blobs)-2
+blob_Class = blob_service.get_blob_to_text(container_name=container_name, blob_name = blobs[TodayNo].name)
+blob_string = blob_Class.content
+blob_df = pd.read_csv(StringIO(blob_string),low_memory=False)
+print(blob_df.head(5))
+
+# the below part is for upload
+#from azure.storage.blob import AppendBlobService
+#import os
+#print(os.getcwd())
+
+#append_blob_service = AppendBlobService(account_name=account_name, account_key=account_key)
+
+# The same containers can hold all types of blobs
+#append_blob_service.create_container(container_name)
+
+# Append blobs must be created before they are appended to
+
+#append_blob_service.create_blob(container_name, 'myappendblob')
+#append_blob_service.append_blob_from_text(container_name, 'myappendblob', u'Hello, world!')
+
+#append_blob = append_blob_service.get_blob_to_text(container_name, 'myappendblob')
+
+
+
+
+
