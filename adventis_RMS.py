470b795e286e6ffe198e2e7c02d5b3bd2d2e777c
lovelysusie <haonanshihaoren@126.com>
2017-04-11 12:11:21 +0800

    made changes

--- adventis_RMS.py
+++ adventis_RMS.py
@@ -25,25 +25,25 @@ table_service = TableService(account_name=myaccount, account_key=mykey)
 
 tasks = table_service.query_entities('sensor', filter="devId eq 'e47fb2f7ae63'and PartitionKey eq '20'")
 
-def setflag(timestamp,day):
+def setflag(timestamp,day,tz):
     flag = date.today()- timedelta(day)
     flag = flag.strftime('%Y-%m-%d')
     flag = flag + ' '+ timestamp
     flag = datetime.strptime(flag, "%Y-%m-%d %H:%M:%S")
-    flag = flag.replace(tzinfo=pytz.timezone('Asia/Singapore'))
+    if tz!='normal':
+        flag = flag.replace(tzinfo=pytz.timezone('Asia/Singapore'))
+    else:
+        flag = flag
     return flag
-flag1 = setflag('20:59:59',1)
-flag2 = setflag('08:30:01',0)
+flag1 = setflag('20:59:59',0,'s')
+flag2 = setflag('08:30:01',0,'s')
 
 rms_list = []
 for task in tasks:
     if (task.Timestamp >flag1) & (task.Timestamp <flag2):
         print(task.Timestamp)
-        #task = Entity()
-        #print(type(task))        
         testing = task.Timestamp
         rms_list.append(testing)
-        #print(getValues(task))
 rms_data = pd.DataFrame()
 rms_data['UTCtime'] = rms_list
 
@@ -78,7 +78,7 @@ if rms_data.shape[0]!=0:
     time_table = time_table.rename(index=str, columns={ 2: "5min"})
     time_table = time_table.set_index(time_table.index.map(parser.parse))
     awake_table = time_table.groupby(pd.TimeGrouper('600s')).max()
-    #awake_table = awake_table[awake_table['5min']>0]
+    awake_table = awake_table[awake_table['5min']>0]
 else:
     awake_table = pd.DataFrame()
 
@@ -105,10 +105,7 @@ blob_table = blob_table[(blob_table['date']==Today)|(blob_table['date']==Yst)]
 
 def merge_data(bathroom_data,RMS_data):
     bathroom_data['timestamp'] = bathroom_data['time'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
-    #clinical_data['clincal_readable_time'] = clinical_data['clincal_readable_time'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
-    #left join sensor and clincal data
     merged_data = bathroom_data.merge(RMS_data, how='left', left_on=bathroom_data['timestamp'],right_on=RMS_data['time'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S')))
-    #forward fill the remaining rows as the clincal data is at the freq of 10s where as sensor data is at the freq if 1s
     return merged_data
 
 #---------------------------------------------------------if there is bathroom data--------------------------------------------
@@ -116,7 +113,7 @@ if blob_table.shape[0]>0:
     blob_df = pd.DataFrame()
     for blobname in blob_table['blobname']:
         blob_Class = blob_service.get_blob_to_text(container_name=container_name, blob_name = blobname)
-        blob_String =blob_Class.content 
+        blob_String =blob_Class.content
         blob_df1 = pd.read_csv(StringIO(blob_String),low_memory=False)
         blob_df = blob_df.append(blob_df1)    
 
@@ -129,13 +126,17 @@ if blob_table.shape[0]>0:
     bathroom_time = blob_df.groupby(pd.TimeGrouper('600s')).max()
     bathroom_time = bathroom_time.dropna(subset=['value'], how='all')
     bathroom_time['eventtime'] = bathroom_time.index
+    bathroom_time.index = range(bathroom_time.shape[0])
+    bathroom_time = bathroom_time[['time']]
+    bathroom_time['1min'] = 0;bathroom_time['sum'] = 0;bathroom_time['5min'] = 0
     blob_Class = None;blob_String = None;blob_date = None;blob_df1 = None
-    del bathroom_time['endtime']; del bathroom_time['name']; del bathroom_time['address']
     #----------add RMS data--------------------------------------------------
     if awake_table.shape[0]>0:
         awake_table['time'] = awake_table.index
         awake_table.index = range(awake_table.shape[0])
-        #awake_table['time'] = awake_table['time'].apply(lambda x: datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+        awake_table = awake_table.append(bathroom_time)
+        awake_table.index = range(awake_table.shape[0])         
+        awake_table = awake_table.drop_duplicates('time',keep='first')#.reset_index(drop=True)
         awake_table['status1'] = 'duration'
         i=1;n = awake_table.shape[0]
         while (i < n):
@@ -145,32 +146,65 @@ if blob_table.shape[0]>0:
             i=i+1
         awake_table.at[n-1,'status1'] = 'start'
         awake_table = awake_table[awake_table['status1']=='start']
+        #-------use HV table filter the bathroom data
+        flag = awake_table['time'];flag.index = range(flag.shape[0])
+        flag1 = flag[0];k = len(flag)-1;flag2 = flag[k]
+        #del awake_table['time']
+        bathroom_time = bathroom_time[(bathroom_time['eventtime']>flag1) & (bathroom_time['eventtime']<flag2)]
+        print(bathroom_time.ix[:0]['eventtime'])
+        print(flag1)
         del awake_table['status1']
         print(awake_table)
-        merged_data = merge_data(bathroom_time, awake_table)
-        
-        '''
-        flag3 = setflag("13:00:00",1)#the real time is 05:00 26
+        if bathroom_time.shape[0]!=0:
+            merged_data = merge_data(bathroom_time, awake_table)
+        #-------------deal with HV talbe & select the wake up time--------------------------
+        flag3 = setflag("05:00:00",1,'normal')
+        awake_table['time'] = awake_table['time'].apply(lambda x: x.to_datetime())
         wakeup = awake_table[awake_table['time']>flag3]
-        if wakeup.shape[0]!=0:
+        
+        if wakeup.shape[0]>1:
             wakeup.index = range(wakeup.shape[0])
-            delta = wakeup.at[0, 'time'] - wakeup.at[1, 'time']
+            delta = wakeup.at[1, 'time'] - wakeup.at[0, 'time']
             if delta.seconds <2400:
                 wakeup = wakeup.iloc[[0]]
             else :
                 wakeup = wakeup.iloc[[1]]
+            print(wakeup)
+            wakeup['hubid'] = 'SG-04-avent001'
+            wakeup = wakeup.rename(index=str, columns={ 'time': 'wakeup'})   
+        if wakeup.shape[0]==1:
             wakeup['hubid'] = 'SG-04-avent001'    
             wakeup = wakeup.rename(index=str, columns={ 'time': 'wakeup'})
             print(wakeup)
-        else:
+        if wakeup.shape[0]==0:
             print("hello, no data")
-        '''
+        #-------------deal with HV talbe & select the sleep time--------------------------
+        flag3 = setflag("03:00:00",0,'normal')
+        sleep = awake_table[awake_table['time']<flag3]
+        if sleep.shape[0]>1:
+            sleep.index = range(sleep.shape[0])
+            delta = sleep.at[1, 'time'] - sleep.at[0, 'time']
+            if delta.seconds <2400:
+                sleep = sleep.iloc[[1]]
+            else :
+                sleep = sleep.iloc[[0]]
+            print(sleep)
+            sleep = sleep.rename(index=str, columns={ 'time': 'sleep'})   
+        if sleep.shape[0]==1:
+            sleep['hubid'] = 'SG-04-avent001'    
+            sleep = sleep.rename(index=str, columns={ 'time': 'sleep'})
+            print(sleep)
+        if sleep.shape[0]==0:
+            print("hello, no data")
+        sleep['hubid'] = 'SG-04-avent001' 
+        sleep = sleep.rename(index=str, columns={ 'time': 'sleep'})
+        
 #---------------------------------------------------------if do not have bathroom data--------------------------------------------
 if blob_table.shape[0]==0:
     if awake_table.shape[0]>0:
         awake_table['time'] = awake_table.index
         awake_table.index = range(awake_table.shape[0])
-        #awake_table['time'] = awake_table['time'].apply(lambda x: datetime.strptime(x,'%Y-%m-%d %H:%M:%S'))
+        awake_table = awake_table.drop_duplicates('time',keep='first')#.reset_index(drop=True)
         awake_table['status1'] = 'duration'
         i=1;n = awake_table.shape[0]
         while (i < n):
@@ -180,24 +214,49 @@ if blob_table.shape[0]==0:
             i=i+1
         awake_table.at[n-1,'status1'] = 'start'
         awake_table = awake_table[awake_table['status1']=='start']
-        del awake_table['status1']
         print(awake_table)
-        flag3 = setflag("13:00:00",1)#the real time is 05:00 26
-        '''
+        #-------------deal with HV talbe & select the wake up time--------------------------
+        print(len(awake_table))
+        flag3 = setflag("05:00:00",0,'normal')
+        awake_table['time'] = awake_table['time'].apply(lambda x: x.to_datetime())
         wakeup = awake_table[awake_table['time']>flag3]
-        if wakeup.shape[0]!=0:
+        
+        if wakeup.shape[0]>1:
             wakeup.index = range(wakeup.shape[0])
-            delta = wakeup.at[0, 'time'] - wakeup.at[1, 'time']
+            delta = wakeup.at[1, 'time'] - wakeup.at[0, 'time']
             if delta.seconds <2400:
                 wakeup = wakeup.iloc[[0]]
             else :
                 wakeup = wakeup.iloc[[1]]
+            print(wakeup)
+            wakeup = wakeup.rename(index=str, columns={ 'time': 'wakeup'})   
+        if wakeup.shape[0]==1:
             wakeup['hubid'] = 'SG-04-avent001'    
             wakeup = wakeup.rename(index=str, columns={ 'time': 'wakeup'})
             print(wakeup)
-        else:
-            print("hello, no bathroom and RMS data")
-        '''
+        if wakeup.shape[0]==0:
+            print("hello, no data")
+        #--------------------deal with sleep time based on HV table------------------------------------
+        flag3 = setflag("03:00:00",0,'normal')
+        sleep = awake_table[awake_table['time']<flag3]
+        if sleep.shape[0]>1:
+            sleep.index = range(sleep.shape[0])
+            delta = sleep.at[1, 'time'] - sleep.at[0, 'time']
+            if delta.seconds <2400:
+                sleep = sleep.iloc[[1]]
+            else :
+                sleep = sleep.iloc[[0]]
+            print(sleep)
+            sleep = sleep.rename(index=str, columns={ 'time': 'sleep'})   
+        if sleep.shape[0]==1:
+            sleep['hubid'] = 'SG-04-avent001'    
+            sleep = sleep.rename(index=str, columns={ 'time': 'sleep'})
+            print(sleep)
+        if sleep.shape[0]==0:
+            print("hello, no data")
+        sleep['hubid'] = 'SG-04-avent001' 
+        sleep = sleep.rename(index=str, columns={ 'time': 'sleep'})
+        
 account_name='blobsensordata'
 account_key='zUYv9mIC9KPr/k+Sa15y4mN6mtozuJcF/n979cqojT4HaMUj3ahEHaPBVtpDihwfO78JTk8sQ29xCaxGWfjtSA=='
 
@@ -208,8 +267,9 @@ container_name = 'rmsdata'
 append_blob_service.create_container(container_name)
 
 sleep_text = awake_table.to_string()
-append_blob_service.create_blob(container_name, 'wholeoutput0406')
-append_blob_service.append_blob_from_text(container_name, 'wholeoutput0406', sleep_text)
+append_blob_service.create_blob(container_name, 'wholeoutput0410')
+append_blob_service.append_blob_from_text(container_name, 'wholeoutput0410', sleep_text)
 merged_data = merged_data.to_string()
-append_blob_service.create_blob(container_name, 'bathroom0406')
-append_blob_service.append_blob_from_text(container_name, 'bathroom0406', merged_data)
+append_blob_service.create_blob(container_name, 'bathroom0410')
+append_blob_service.append_blob_from_text(container_name, 'bathroom0410', merged_data)
+
