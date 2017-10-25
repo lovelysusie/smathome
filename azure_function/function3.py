c561034bc1fd8cc46312b6ec4961e350a6fe562b
lovelysusie <haonanshihaoren@126.com>
2017-10-25 16:18:01 +0800

    made changes

--- azure_function/function3.py
+++ azure_function/function3.py
@@ -11,17 +11,6 @@ from dateutil import parser
 from datetime import datetime
 from datetime import timedelta
 
-def get_grouped(rawdata):
-    rawdata = rawdata.set_index(rawdata['eventtime'].map(parser.parse))
-    room_frame = rawdata.groupby([pd.TimeGrouper('300s'),'deviceid','tasklocation']).max()
-    room_frame['index1'] = room_frame.index#.apply(lambda x: list(x))
-    room_frame['index1'] = room_frame['index1'].apply(lambda x: list(x))
-    room_frame['time'] = room_frame['index1'].apply(lambda x: x[0])
-    room_frame['hub_id'] = room_frame['index1'].apply(lambda x: x[1])
-    room_frame['tasklocation'] = room_frame['index1'].apply(lambda x: x[2])
-    room_frame = room_frame[['hub_id','time','tasklocation','value']]
-    room_frame.index = range(len(room_frame))
-    return room_frame
     
 
 def data_from_blob(container_name_input,blob_name,types='main'):
@@ -35,59 +24,35 @@ def data_from_blob(container_name_input,blob_name,types='main'):
         blob_df_whole['time'] = blob_df_whole['time'].apply(lambda x: datetime.strptime(x,"%Y-%m-%d %H:%M:%S")) 
     return blob_df_whole
 
-def bathroon_checker(bathroom_input):
-    # This function is going to eleminate the sunddently appear value 1 bathroom time stamp
-    bathroom_input['gap'] = bathroom_input['time'].diff() 
-    bathroom_input['gap'][0] = timedelta(minutes = 5)
-    bathroom_input['gap'] = bathroom_input['gap'].apply(lambda x: x.seconds)
-    return bathroom_input[(bathroom_input['sum']!=1)|(bathroom_input['gap']==300)]
-
-
-def awake_table_bathroom(hub_id,room_acttime_input,blob_name):
-    awake_table = data_from_blob('temp1', blob_name)
-    bathroom_time = bathroom_table_prep(room_acttime_input)
-    awake_table.append(bathroom_time)
-    return awake_table
+def del_neighbor(data,types):
+    if len(data)>0:
+        data = data.drop_duplicates('time',keep='first')#.reset_index(drop=True)
+        data = data.sort_values(['time'], ascending=[1])
+        data.index = range(data.shape[0])       
+        if types == 'wakeup':
+            data['gap'] = data['time'].diff()
+            data['gap'].ix[0] = timedelta(seconds=1000)
+            data = data[data['gap']>timedelta(seconds=300)]
+        if types == 'sleep':
+            test = data['time'].diff().tolist()
+            test.pop(0)
+            test.append(timedelta(seconds=300))        
+            data['gap'] = test
+            data = data[data['gap']>timedelta(seconds=300)]
+    return data
 
 
 account_name='####'
 account_key='####'
-
-Today = date.today().strftime('%Y-%m-%d')
-
-container_name = 'pirinputclean'
-blob_df_whole = data_from_blob(container_name,Today)
-
-
 append_blob_service = AppendBlobService(account_name=account_name, account_key=account_key)
 
-room_acttime_whole = get_grouped(blob_df_whole)
-append_blob_service.create_blob('temp2', Today+"_roomacttime")
-append_blob_service.append_blob_from_text('temp2', Today+"_roomacttime", room_acttime_whole.to_csv())
+#Today = date.today().strftime('%Y-%m-%d')
+
+bathroom_whole = data_from_blob('function','bathroom')
+awake_table_whole = data_from_blob('function','awake_table_whole')
 
 
-def bathroom_checker(bathroom_input):
-    # This function is going to eleminate the sunddently appear value 1 bathroom time stamp
-    # bathroom_input  = bathroom_input.sort(['hub_id'], ascending = [1])
-    if len(bathroom_input):
-        bathroom_input['gap'] = bathroom_input['time'].diff() 
-        bathroom_input['gap'][0] = timedelta(minutes = 5)
-        bathroom_input['gap'] = bathroom_input['gap'].apply(lambda x: x.seconds)
-        return bathroom_input[(bathroom_input['sum']!=1)|(bathroom_input['gap']==300)]
-    else:
-        pass
-
-def bathroom_table_prep(room_acttime_input):
-    '''
-    This function is going to get the bathroom raw data, which has not been filtered by the awake table
-    NOTE: here delete 5 min for each tampstam so that when checking all room blank not getting confused
-    '''
-    bathroom_time = room_acttime_input[room_acttime_input['tasklocation']=='Bathroom']
-    del bathroom_time['tasklocation']
-    bathroom_time = bathroom_time.rename(index=str, columns={'value': "sum"})
-    bathroom_time['gap'] = 0
-    bathroom_time['time'] = bathroom_time['time'].apply(lambda x: x-timedelta(minutes = 5))
-    return bathroom_time
+#append_blob_service = AppendBlobService(account_name=account_name, account_key=account_key)
 
 
 hublist = [
@@ -118,17 +83,32 @@ hublist = [
     'SG-04-hub00009'
 ]
 
-bathroom = bathroom_table_prep(room_acttime_whole)
-bathroom_table = pd.DataFrame()
+wakeup_whole = pd.DataFrame()
+sleep_whole = pd.DataFrame()
 
 for hub in hublist:
-    bathroom_check = bathroom[bathroom['hub_id']==hub]
-    bathroom_check = bathroom_checker(bathroom_check)
-    bathroom_table = bathroom_table.append(bathroom_check)
+    bathroom = bathroom_whole[bathroom_whole['deviceid'] == hub]
+    awake_table = awake_table_whole[awake_table_whole['deviceid'] == hub]
+    awake_table = awake_table.append(bathroom)
+    awake_table_wakeup = del_neighbor(awake_table,'wakeup')
+    wakeup_whole = wakeup_whole.append(awake_table_wakeup)
+    
+    awake_table_sleep = del_neighbor(awake_table,'sleep')
+    sleep_whole = wakeup_whole.append(awake_table_sleep)
+
+append_blob_service.create_blob('function', "awake_table_wakeup")
+wakeup_whole['gap'] = wakeup_whole['gap'].apply(lambda x: x.seconds) 
+
+append_blob_service.append_blob_from_text('function', "awake_table_wakeup", wakeup_whole.to_csv())
+
+sleep_whole['gap'] = sleep_whole['gap'].apply(lambda x: x.seconds) 
+append_blob_service.create_blob('function', "awake_table_sleep")
+append_blob_service.append_blob_from_text('function', "awake_table_sleep", sleep_whole.to_csv())
 
 
-append_blob_service.create_blob('temp2', Today+"_bathroom_whole")
-append_blob_service.append_blob_from_text('temp2', Today+"_bathroom_whole", bathroom_table.to_csv())
+
+
+
 
 
 
